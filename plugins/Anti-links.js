516725207e00a5970ed001c383a10b24bcdf3f4e
The Lolibot-MD <107805973+elrebelde21@users.noreply.github.com>
2023-01-11 22:42:08 -0300

    Update Anti-links.js

--- plugins/Anti-links.js
+++ plugins/Anti-links.js
@@ -9,17 +9,18 @@ let bang = m.key.id
 let bot = global.db.data.settings[this.user.jid] || {}
 const isGroupLink = linkRegex.exec(m.text)
 const grupo = `https://chat.whatsapp.com`
-if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply('*💎ᴇʀᴇs ᴀᴅᴍɪɴ ᴅᴇʟ ɢʀᴜᴘᴏ ᴛᴇ ᴛᴇ ʜᴀs sᴀʟᴠᴀᴅᴏ ᴅᴇ sᴇʀ ᴇʟɪᴍɪɴᴀᴅᴏ*')
+if (isAdmin && chat.antiLink && m.text.includes(grupo)) return m.reply('*𝙷𝙴𝚈!! 𝙴𝙻 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺 𝙴𝚂𝚃𝙰 𝙰𝙲𝚃𝙸𝚅𝙾, 𝙿𝙴𝚁𝙾 𝙴𝚁𝙴𝚂 𝚄𝙽 𝙰𝙳𝙼𝙸𝙽 😎, 𝚂𝙰𝙻𝚅𝙰𝙳𝙾/𝙰!*')
 if (chat.antiLink && isGroupLink && !isAdmin) {
 if (isBotAdmin) {
 const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
 if (m.text.includes(linkThisGroup)) return !0
 }    
-await conn.sendButton(m.chat, `*ʟᴇ ❗ɪɴғᴏʀᴍᴏ ǫᴜᴇ ʜᴀ ʀᴏᴍᴘɪᴅᴏ ʟᴀs ʀᴇɢʟᴀs sᴇʀᴀ ᴇʟɪᴍɪɴᴀᴅᴏ ᴅᴇ ɪɴᴍᴇᴅɪᴀᴛᴏ☘️* ${await this.getName(m.sender)} *ʜᴀsᴛᴀ ʟᴀ ᴘʀᴏxɪᴍᴀ👋*${isBotAdmin ? '' : '\n❗] ᴇsᴛᴇ ʙᴏᴛ ɴᴏ ᴇs ᴀᴅᴍɪɴ ᴘᴀʀᴀ ᴘᴏᴅᴇʀ ᴇʟɪᴍɪɴᴀʀ'}`, author, ['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺𝚂', '/disable antilink'], m)    
+await conn.sendButton(m.chat, `*「 𝐀𝐍𝐓𝐈 𝐋𝐈𝐍𝐊𝐒 」*\n*𝙷𝙰𝚂𝚃𝙰 𝙻𝙰 𝚅𝙸𝚂𝚃𝙰 𝙱𝙰𝙱𝚈 👋, ${await this.getName(m.sender)} 𝚁𝙾𝙼𝙿𝙸𝚂𝚃𝙴𝚂 𝙻𝙰𝚂 𝚁𝙴𝙶𝙻𝙰𝚂 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾, 𝚂𝙴𝚁𝙰𝚂 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝙳𝙾...!!*${isBotAdmin ? '' : '\n\n*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝙴𝚂 𝙰𝙳𝙼𝙸𝙽, 𝙽𝙾 𝙿𝚄𝙴𝙳𝙴 𝙴𝚇𝚃𝙴𝚁𝙼𝙸𝙽𝙰𝚁 𝙰 𝙻𝙰𝚂 𝙿𝙴𝚁𝚂𝙾𝙽𝙰𝚂*'}`, author, ['𝙳𝙴𝚂𝙰𝙲𝚃𝙸𝚅𝙰𝚁 𝙰𝙽𝚃𝙸𝙻𝙸𝙽𝙺𝚂', '/disable antilink'], m)    
 if (isBotAdmin && bot.restrict) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet }})
-await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-} else if (!bot.restrict) return m.reply('*[❗]ᴇʟ ɪɴғᴏʀᴍᴏ ǫᴜᴇ ᴘᴀʀᴀ ǫᴜᴇ ᴇsᴛᴇ ᴄᴏᴍᴀɴᴅᴏ ғᴜɴᴄɪᴏɴᴇ ᴛɪᴇɴᴇs ǫᴜᴇ ᴀᴄᴛɪᴠᴀʀ ᴇʟ ᴄᴏᴍᴀɴᴅᴏ [ᴇɴᴀʙʟᴇ  ʀᴇsᴛʀɪᴄᴛ] sᴏʟᴏ ғᴜɴᴄɪᴏɴᴀ ᴘᴀʀᴀ ᴇʟ ᴀᴅᴍɪɴ ᴅᴇ ɢʀᴜᴘᴏ ᴏ ᴇʟ ᴘʀᴏᴘɪᴇᴛᴀʀɪᴏ ᴅᴇʟ ʙᴏᴛ*')
+let responseb = await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+if (responseb[0].status === "404") return   
+} else if (!bot.restrict) return m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃 𝙽𝙾 𝚃𝙸𝙴𝙽𝙴 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙰𝙳𝙾 𝙻𝙰𝚂 𝚁𝙴𝚂𝚃𝚁𝙸𝙲𝙲𝙸𝙾𝙽𝙴𝚂 (#𝚎𝚗𝚊𝚋𝚕𝚎 𝚛𝚎𝚜𝚝𝚛𝚒𝚌𝚝) 𝙲𝙾𝙽𝚃𝙰𝙲𝚃𝙴 𝙲𝙾𝙽 𝙴𝙻 𝙿𝙰𝚁𝙰 𝚀𝚄𝙴 𝙻𝙾 𝙷𝙰𝙱𝙸𝙻𝙸𝚃𝙴*')
 }
 return !0
 }
