db423e0de4a2d332f6b58e567ad783f415b87f67
TOLXM <151574123+TOLXM@users.noreply.github.com>
2023-11-20 17:13:26 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
-global.owner = [
+global.owner = ['91138781179', '𝙾𝙵𝙲 𝚈𝙾𝚅𝙰𝙽𝙸', true]
   ['5212411719888', '𝙾𝙵𝙲 𝚈𝙾𝚅𝙰𝙽𝙸', true],
   ['5212412377467', '𝙾𝙵𝙲 𝚈𝙾𝚅𝙰𝙽𝙸', true],
   ['5212411347465', '𝙾𝙵𝙲 𝚈𝙾𝚅𝙰𝙽𝙸', true],
